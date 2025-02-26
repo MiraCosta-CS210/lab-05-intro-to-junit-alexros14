6112c4962582303609f452f5bfc69128a82b4b76
alexros14 <alexrosals377@gmail.com>
2025-02-25 22:14:05 -0800

    Update TipCalculatorTest.java
    ready to grade!

--- tests/TipCalculatorTest.java
+++ tests/TipCalculatorTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TipCalculatorTest {
+    @Test
+    void calculateTip() {
+        TipCalculator test = new TipCalculator();
+        double result = test.calculateTip(100,20);
+        assertEquals(20,result);
+    }
+
+    @Test
+    void calculateTip2() {
+        TipCalculator test = new TipCalculator();
+        double result = test.calculateTip(50,50);
+        assertEquals(25,result);
+    }
 }
