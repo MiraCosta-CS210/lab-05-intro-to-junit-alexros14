ea1b5630cc00babf8e918218a560201a8dd16627
alexros14 <alexrosals377@gmail.com>
2025-02-25 22:14:21 -0800

    Update SumNumbersTest.java

--- tests/SumNumbersTest.java
+++ tests/SumNumbersTest.java
@@ -1,2 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SumNumbersTest {
+    @org.junit.jupiter.api.Test
+    void sumIsEven() {
+        SumNumbers test = new SumNumbers();
+        int result = test.sumIsEven(1,1);
+        assertEquals(1,result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumIsEven2() {
+        SumNumbers test = new SumNumbers();
+        int result = test.sumIsEven(1,2);
+        assertEquals(0,result);
+    }
 }
